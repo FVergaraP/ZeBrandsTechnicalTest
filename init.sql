edaa85f838f0c1f490f0951922247b381785452c
fjvergarap <fjvergarap@falabella.cl>
2024-05-05 16:01:19 -0400

    chore: update init.sql to create table products and add initial product

--- init.sql
+++ init.sql
@@ -8,3 +8,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 );
 
 INSERT INTO `users` (`email`, `full_name`, `password`, `superadmin`) VALUES ('tpogacar@giro.it', 'Tadej Pogacar', '$2b$12$T64sPBGjyf6nIxHe5y74a.vHTYx4sTEu1aYASWeXpyzpVsDzRcsrO', 1);
+
+CREATE TABLE IF NOT EXISTS `products` (
+    `sku` varchar(100) PRIMARY KEY NOT NULL UNIQUE,
+    `name` varchar(250) NOT NULL,
+    `price` real NOT NULL,
+    `brand` varchar(250) NOT NULL
+);
+
+INSERT INTO `products`(`sku`,`name`,`price`,`brand`) VALUES ('212121', 'Colchon Signature', 29000, 'Luuna')
